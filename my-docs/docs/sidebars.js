f5141bf6c55dd4609c7b2399765f43a58bca80a1
Paweł Żebrowski <pawel.zebrowski@studenci.collegiumwitelona.pl>
2025-01-25 19:13:22 +0100

    dsada

--- my-docs/docs/sidebars.js
+++ my-docs/docs/sidebars.js
@@ -2,19 +2,15 @@ module.exports = {
   tutorialSidebar: [
     {
       type: 'category',
-      label: 'Planer Ślubu', // Tytuł główny kategorii
+      label: 'Planer Ślubu',
       items: [
-        'backloge', // Przeniesiono na pierwsze miejsce
-        'estimate', // Przeniesiono na drugie miejsce
-        {
-          type: 'category',
-          label: 'Backlog Funkcjonalności', // Polskie znaki w label
-          items: ['features'], // Zawiera tylko 'features'
-        },
+        'backloge',
+        'estimate',
+        'features',
         'user_role',
         'architecture',
         'installation',
-        'intro', // Dodano 'intro' na końcu
+        'intro',
       ],
     },
   ],
